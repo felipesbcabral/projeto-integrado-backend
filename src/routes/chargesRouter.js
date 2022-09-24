982844c450ddff871320ca0d67b991513170bfb4
jhondutrati <jhondutrati@gmail.com>
2022-09-24 19:40:36 -0300

    changes controllers and routes

--- src/routes/chargesRouter.js
+++ src/routes/chargesRouter.js
@@ -8,4 +8,10 @@ router.get('/', chargesController.listarContas );
 
 router.get('/:id', chargesController.listarContasPorId );
 
+router.put('/:id',chargesController.atualizarContaPorId);
+
+router.post('/', chargesController.criarCobranca);
+
+router.delete('/:id', chargesController.deletarCobranca);
+
 module.exports = router;
